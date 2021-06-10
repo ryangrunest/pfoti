7fd8447ce3b77c8328eded1e4540e5e088cc9387
Ryan Grunest <ryangrunest@geeko>
2021-06-10 10:01:10 -0700

    refactor defaultheader as separate component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,47 +1,24 @@
 <template>
 	<v-app>
-		<v-app-bar
-			absolute
-			color="#fcb69f"
-			dark
-			app
-			src="https://images.unsplash.com/photo-1605940169841-60884072a854?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2748&q=80"
-		>
-			<v-app-bar-nav-icon></v-app-bar-nav-icon>
-			<v-app-bar-title class="px-0">
-				<div class="text-h5">
-					pfoti <span class="text-subtitle-1">- pagefront of the internet</span>
-				</div>
-			</v-app-bar-title>
-			<v-spacer></v-spacer>
-			<v-btn icon>
-				<v-icon>mdi-magnify</v-icon>
-			</v-btn>
-			<v-btn icon @click="toggleModal">
-				<v-icon>mdi-cog-outline</v-icon>
-			</v-btn>
-		</v-app-bar>
+		<DefaultHeader />
 		<v-main class="mt-4">
-			<SettingsModal></SettingsModal>
-
+			<SettingsModal />
 			<Nuxt />
 		</v-main>
 	</v-app>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import DefaultHeader from './components/DefaultHeader.vue'
 import SettingsModal from './components/SettingsModal.vue'
 
 export default {
 	components: {
+		DefaultHeader,
 		SettingsModal,
 	},
 	data() {
 		return {}
-	},
-	methods: {
-		...mapActions(['toggleModal']),
 	},
 }
 </script>
